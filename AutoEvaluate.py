7f4d9d1ea97342f412415dc8c60ef020c927e8a7
sdogsq <8475900@qq.com>
2019-12-10 14:36:33 +0800

    19fall

---
+++ AutoEvaluate.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+import re
+import time
+from PIL import Image
+from io import BytesIO
+import matplotlib.pyplot as plt 
+from bs4 import BeautifulSoup
+# 请求头信息
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding':'gzip, deflate',
+    'Accept-Language': 'en',
+    'Cache-Control': 'max-age=0',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'DNT':'1',
+    'Host':'sep.ucas.ac.cn',
+    'Origin': 'http://sep.ucas.ac.cn',
+    'Referer': 'http://sep.ucas.ac.cn/',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+}
+
+regex = re.compile(r"'(.*?)'")
+
+sep_link = "http://sep.ucas.ac.cn/slogin"
+
+params_nocode = {'userName':'Your User Name', # 填入用户名
+          'pwd':'Your password', #填入密码
+          'sb':'sb'}
+
+
+params_withcode = {'userName':'Your User Name',#填入用户名
+          'pwd':'Your password', #填入密码
+          'certCode': '',
+          'sb':'sb'}
+
+
+def localtime():
+    return time.asctime( time.localtime(time.time()) )
+
+
+
+s = requests.Session()
+c = requests.Session()
+
+# ------------------- login sep -------------------------
+s = requests.Session()
+Sep_Login = s.post(url=sep_link, data=params_nocode,headers=headers,verify=False,timeout=10) # login
+sl = BeautifulSoup(Sep_Login.text, 'lxml')
+
+if (sl.find('input', attrs={'name':'certCode'}) !=None):  # verCode needed
+    vpic = s.get('http://sep.ucas.ac.cn/changePic?code='+str(int(round(time.time() * 1000))))
+    image = Image.open(BytesIO(vpic.content))
+    #image.show()
+    plt.imshow(image)
+    plt.show()
+    
+    #SendEmail(' 需要登录验证码 ','000')
+    vcode = input()
+    
+    
+    params_withcode['certCode'] = vcode
+    Sep_Login = s.post(url=sep_link, data=params_withcode,headers=headers,verify=False,timeout=10) 
+    sl = BeautifulSoup(Sep_Login.text, 'lxml')
+
+if (sl.find('a',title='退出系统') == None): # check login status
+    raise Exception("Sep Login Error")
+else:
+    print(" SEP LOGIN SUCCESS")
+# -------------------------------------------------------
+
+# -------------------  login jwxk ------------------------ 
+jwxk = s.get('http://sep.ucas.ac.cn/portal/site/226/821')
+jwxk = BeautifulSoup(jwxk.text, 'lxml')
+j_link = jwxk.noscript.meta.attrs['content'][6:]
+
+c = requests.Session()
+j_login = c.get(url=j_link,cookies = s.cookies.get_dict())
+requests.utils.add_dict_to_cookiejar(c.cookies, {'sepuser': s.cookies.get_dict()['sepuser']}) # set jwxk cookies
+# -------------------------------------------------------
+
+
+# ----------------- Evaluate Course ---------------------
+cdata = { # Post Data
+    'subjectiveRadio': 22,
+    'subjectiveCheckbox': 28,
+    'item_2': 5,
+    'item_3': 5,
+    'item_67': 5,
+    'item_5': 5,
+    'item_6': 5,
+    'item_7': 5,
+    'item_68': 5,
+    'item_69': 5,
+    'item_71': 5,
+    'item_72': 5,
+    'item_73': 5,
+    'item_74': 5,
+    'item_75': 5,
+    'item_77': 5,
+    'item_78': 5,
+    'item_79': 5,
+    'item_80': 5,
+    'item_81': 5,
+    'item_83': 5,
+    'item_84': 5,
+    'item_85': 5,
+    'item_86': 5,
+    'item_14': '作业（包括作业、报告、测验测试、论文等）有助于我的能力的提高',
+    'item_15': '作业（包括作业、报告、测验测试、论文等）有助于我的能力的提高',
+    'item_16': '作业（包括作业、报告、测验测试、论文等）有助于我的能力的提高',
+    'item_17': '作业（包括作业、报告、测验测试、论文等）有助于我的能力的提高',
+    'item_18': '作业（包括作业、报告、测验测试、论文等）有助于我的能力的提高',
+    'radio_19': '',
+    'item_25': ''
+}
+
+EvaPage = c.get("http://jwxk.ucas.ac.cn/evaluate/course/59585")
+
+eva = BeautifulSoup(EvaPage.text, 'lxml')
+
+hrefnum = re.compile(r"(\d+)")
+for a in eva.find_all('a', string='修改评估', href=True):
+    CourseNum = hrefnum.findall(a['href'])[0]
+    res = c.post("http://jwxk.ucas.ac.cn/evaluate/saveCourseEval/" + CourseNum,data = cdata)
+    print(CourseNum)
+    time.sleep(1)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # UCAS-Course-Evaluate-19Fall
+
+课程自动评估，2019秋季学期使用，学期更换会导致评估页面`EvaPage`地址改变。
+
+#### 使用方法
+
+- 在参数`params_nocode`与`params_withcode`中填入SEP用户名及密码。
+
+- 在参数`cdata`中修改课程评语。
+
+- 运行程序。若在UCAS内网运行无需输入验证码，若在外网运行，运行后会自动弹出验证码，输入即可。
